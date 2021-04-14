7325339cb42457bedfbb291cbe5edb3d34bd4ede
Jay <j.sutton@vic.chariot.net.au>
2021-04-14 23:40:21 +1000

    Need to put all update function into single JS so they can all work off the same listening event- putting all update

--- static/js/applineplot.js
+++ static/js/applineplot.js
@@ -128,7 +128,7 @@ function runEnter() {
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#selDataset");
   var inputValue = inputElement.property("value");
-  console.log(`Filter: ${inputValue}`);
+  console.log(` DIANA Filter: ${inputValue}`);
   console.log('--------')
 
   //Run function to update sunburst
@@ -136,111 +136,119 @@ function runEnter() {
 
 } 
 
-function updatelineplot(indexnumber) {
-  console.log("diana",indexnumber);
+// function updatelineplot(indexnumber) {
+//   console.log("diana",indexnumber);
 
-  var url = `/${indexnumber}`
+//   var url = `/${indexnumber}`
 
-  d3.json(url).then(function(data) {
-    var Xaxis = [];
-    for (var i = 0; i < data.length; i++) {
-      var currentdate = data[i].post_date;
-      if(Xaxis.indexOf(currentdate) == -1){
-        Xaxis.push(currentdate);
-      }
-    }
-    var likeslist = []
-    var commentslist = []
-    var retweetslist =[]
+//   d3.json(url).then(function(data) {
+//     var Xaxis = [];
+//     // console.log(data);
+//     for (var i = 0; i < data.length; i++) {
+//       var currentdate = data[i].post_date;
+//       // console.log(currentdate)
+//       if(Xaxis.indexOf(currentdate) == -1){
+//         Xaxis.push(currentdate);
+//       }
+//     } 
+//     var likeslist = []
+//     var commentslist = []
+//     var retweetslist =[]
 
-    for (var i = 0; i < Xaxis.length; i++) {
-      // console.log(Xaxis[i])
-      sumlikes = 0
-      sumcomments = 0
-      sumretweets = 0
-//         //This uses the value in XAxis list to look through the full data and to find all rows with matching dates
-      for (var j = 0; j < data.length; j++) {
-//           //grab each value in turn from the reaction_total column (or change to other column)
-        currentlikes = data[j].like_num;
-        currentcomments = data[j].comment_num;
-        currentretweets = data[j].retweet_num;
-        // console.log(currentlikes)
-//           //grab each value in turn from the relevant column (ie you would use date column)
-        datecurrent = data[j].post_date;
-        // console.log(i, datecurrent)
-//           //So if the first item in your list of XAxis (ie the date) = the current date selected in this loop
-        if (Xaxis[i] == datecurrent){
-//             // console.log(writer[i])
+//     for (var i = 0; i < Xaxis.length; i++) {
+//       // console.log(Xaxis[i])
+//       sumlikes = 0
+//       sumcomments = 0
+//       sumretweets = 0
+      
+//       //This uses the value in XAxis list to look through the full data and to find all rows with matching dates
+//       for (var j = 0; j < data.length; j++) {
+//         //grab each value in turn from the reaction_total column (or change to other column)
+//         currentlikes = data[j].like_num;
+//         currentcomments = data[j].comment_num;
+//         currentretweets = data[j].retweet_num;
+//         // console.log(currentlikes);
+//         // console.log(currentcomments);
+//         // console.log(currentretweets);
+
+//         //grab each value in turn from the relevant column (ie you would use date column)
+//         datecurrent = data[j].post_date;
+//         // console.log(i, datecurrent)
+        
+//         //So if the first item in your list of XAxis (ie the date) = the current date selected in this loop
+//         if (Xaxis[i] == datecurrent){
+//             // console.log(datecurrent[i])
 //             //Add the current value to the sum variable = so you get a running tally for that date
-          sumlikes += currentlikes;
-          sumcomments += currentcomments;
-          sumretweets += currentretweets;
-//             // console.log(holding[i], “is equals”);
-        }
-      }
-      likeslist.push(sumlikes);
-      commentslist.push(sumcomments);
-      retweetslist.push(sumretweets);
+//           sumlikes += currentlikes;
+//           sumcomments += currentcomments;
+//           sumretweets += currentretweets;
+//           // console.log(sumretweets);
+//         }
+//       }
+//       likeslist.push(sumlikes);
+//       commentslist.push(sumcomments);
+//       retweetslist.push(sumretweets);
 
-    }
-    console.log(Xaxis);
-    console.log(likeslist);
-    console.log(commentslist);
-    console.log(retweetslist);
+//     }
+//     console.log(Xaxis);
+//     console.log(likeslist);
+//     console.log(commentslist);
+//     console.log(retweetslist);
 
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      name: "comments data",
-      x: Xaxis,
-      y: commentslist,
-      line: {
-          color: "#17BECF"
-        }
+//     var trace1 = {
+//       type: "scatter",
+//       mode: "lines",
+//       name: "comments data",
+//       x: Xaxis,
+//       y: commentslist,
+//       line: {
+//         color: "#17BECF"
+//           }
 
-  }
+//     }
 
-  var trace2 = {
-      type: "scatter",
-      mode: "lines",
-      name: "retweets data",
-      x: Xaxis,
-      y: retweetslist,
-      line: {
-        color: "#1F77B4"
-      }
-  }
-
-
-  var trace3 = {
-      type: "scatter",
-      mode: "lines",
-      name: "likes data",
-      x: Xaxis,
-      y: likeslist,
-      line: {
-        color: "#FF7F0E"
-      }
-    };
+//   var trace2 = {
+//     type: "scatter",
+//     mode: "lines",
+//     name: "retweets data",
+//     x: Xaxis,
+//     y: retweetslist,
+//     line: {
+//       color: "#1F77B4"
+//     }
+//   }
 
 
-  var data = [trace1, trace2, trace3];
+//   var trace3 = {
+//       type: "scatter",
+//       mode: "lines",
+//       name: "likes data",
+//       x: Xaxis,
+//       y: likeslist,
+//       line: {
+//         color: "#FF7F0E"
+//       }
+//     };
 
-  var layout = {
-      title: "May Twitter Activity",
-      xaxis: {
-          autorange: true,
-          type: "date"
-        },
-        yaxis: {
-          autorange: true,
-          type: "linear"
-        },
-        showlegend: false
-  };
 
-  Plotly.newPlot("lineplot", data, layout);
+//   var data = [trace1, trace2, trace3];
+//   console.log(data)
 
-});
-}
+//   var layout = {
+//       title: "May Twitter Activity",
+//       xaxis: {
+//           autorange: true,
+//           type: "date"
+//         },
+//         yaxis: {
+//           autorange: true,
+//           type: "linear"
+//         },
+//         showlegend: false
+//   }
+
+//   Plotly.newPlot("lineplot", data, layout);
+
+// });
+// }
 
--- static/js/appsunburst.js
+++ static/js/appsunburst.js
@@ -149,6 +149,7 @@ function runEnter() {
 
   //Run function to update sunburst
   updatesunplot(inputValue);
+  updatelineplot(inputValue);
 
 } 
 
@@ -278,6 +279,122 @@ function updatesunplot(indexnumber) {
 
   });
 
+}
+
+function updatelineplot(indexnumber) {
+  console.log("diana",indexnumber);
+
+  var url = `/${indexnumber}`
+
+  d3.json(url).then(function(data) {
+    var Xaxis = [];
+    // console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var currentdate = data[i].post_date;
+      // console.log(currentdate)
+      if(Xaxis.indexOf(currentdate) == -1){
+        Xaxis.push(currentdate);
+      }
+    } 
+    var likeslist = []
+    var commentslist = []
+    var retweetslist =[]
+
+    for (var i = 0; i < Xaxis.length; i++) {
+      // console.log(Xaxis[i])
+      sumlikes = 0
+      sumcomments = 0
+      sumretweets = 0
+      
+      //This uses the value in XAxis list to look through the full data and to find all rows with matching dates
+      for (var j = 0; j < data.length; j++) {
+        //grab each value in turn from the reaction_total column (or change to other column)
+        currentlikes = data[j].like_num;
+        currentcomments = data[j].comment_num;
+        currentretweets = data[j].retweet_num;
+        // console.log(currentlikes);
+        // console.log(currentcomments);
+        // console.log(currentretweets);
+
+        //grab each value in turn from the relevant column (ie you would use date column)
+        datecurrent = data[j].post_date;
+        // console.log(i, datecurrent)
+        
+        //So if the first item in your list of XAxis (ie the date) = the current date selected in this loop
+        if (Xaxis[i] == datecurrent){
+            // console.log(datecurrent[i])
+            //Add the current value to the sum variable = so you get a running tally for that date
+          sumlikes += currentlikes;
+          sumcomments += currentcomments;
+          sumretweets += currentretweets;
+          // console.log(sumretweets);
+        }
+      }
+      likeslist.push(sumlikes);
+      commentslist.push(sumcomments);
+      retweetslist.push(sumretweets);
+
+    }
+    console.log(Xaxis);
+    console.log(likeslist);
+    console.log(commentslist);
+    console.log(retweetslist);
+
+    var trace1 = {
+      type: "scatter",
+      mode: "lines",
+      name: "comments data",
+      x: Xaxis,
+      y: commentslist,
+      line: {
+        color: "#17BECF"
+          }
+
+    }
+
+  var trace2 = {
+    type: "scatter",
+    mode: "lines",
+    name: "retweets data",
+    x: Xaxis,
+    y: retweetslist,
+    line: {
+      color: "#1F77B4"
+    }
+  }
+
+
+  var trace3 = {
+      type: "scatter",
+      mode: "lines",
+      name: "likes data",
+      x: Xaxis,
+      y: likeslist,
+      line: {
+        color: "#FF7F0E"
+      }
+    };
+
+
+  var data = [trace1, trace2, trace3];
+  console.log(data)
+
+  var layout = {
+      title: "May Twitter Activity",
+      xaxis: {
+          autorange: true,
+          type: "date"
+        },
+        yaxis: {
+          autorange: true,
+          type: "linear"
+        },
+        showlegend: false
+  }
+
+  Plotly.newPlot("lineplot", data, layout);
+
+});
 }
 // sunplot(); - don't need this to run update chart as it isbeing called buy listening event
 
